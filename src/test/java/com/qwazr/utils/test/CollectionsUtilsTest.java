d3021e8ac66c3785768ff317ecf91aea17b8d6c7
Emmanuel Keller <ekeller@open-search-server.com>
2017-08-11 14:12:19 +0200

    Add CollectionsUtils tests

--- src/test/java/com/qwazr/utils/test/CollectionsUtilsTest.java
+++ src/test/java/com/qwazr/utils/test/CollectionsUtilsTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2016-2017 Emmanuel Keller / QWAZR
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,51 +31,90 @@ import java.util.TreeMap;
 public class CollectionsUtilsTest {
 
 	@Test
-	public void notSameSize() {
-		Assert.assertFalse(CollectionsUtils.equals(Arrays.asList(1, 2, 3), Arrays.asList(1, 2)));
+	public void collColl() {
+
+		final Collection<Integer> collSameLeft = Arrays.asList(1, 2, 3);
+		final Collection<Integer> collSameRight = Arrays.asList(1, 2, 3);
+		final Collection<Integer> collOtherSameSize = Arrays.asList(3, 2, 1);
+		final Collection<Integer> collOtherDifferentSize = Arrays.asList(1, 2);
+		final Collection<Integer> collNull = null;
+
+		// Same null
+		Assert.assertTrue(CollectionsUtils.equals(null, collNull));
+		// Same reference
+		Assert.assertTrue(CollectionsUtils.equals(collSameLeft, collSameLeft));
+		// Same content
+		Assert.assertTrue(CollectionsUtils.equals(collSameLeft, collSameRight));
+		// Null pos2
+		Assert.assertFalse(CollectionsUtils.equals(collSameLeft, collNull));
+		// Null pos1
+		Assert.assertFalse(CollectionsUtils.equals(collNull, collSameRight));
+		// Not same content
+		Assert.assertFalse(CollectionsUtils.equals(collSameLeft, collOtherSameSize));
+		// Not same content size
+		Assert.assertFalse(CollectionsUtils.equals(collSameLeft, collOtherDifferentSize));
 	}
 
 	@Test
-	public void firstIsNull() {
-		Assert.assertFalse(CollectionsUtils.equals(Arrays.asList(1, 2, 3), (Collection) null));
+	public void collArray() {
+
+		final Collection<Integer> coll = Arrays.asList(1, 2, 3);
+		final Integer[] array = new Integer[] { 1, 2, 3 };
+		final Integer[] arraySameSize = new Integer[] { 3, 2, 1 };
+		final Integer[] arrayDifferentSize = new Integer[] { 1, 2 };
+		final Integer[] arrayNull = null;
+
+		// Same null
+		Assert.assertTrue(CollectionsUtils.equals(null, arrayNull));
+		// Same content
+		Assert.assertTrue(CollectionsUtils.equals(coll, array));
+		// Null pos2
+		Assert.assertFalse(CollectionsUtils.equals(coll, arrayNull));
+		// Null pos1
+		Assert.assertFalse(CollectionsUtils.equals(null, array));
+		// Not same content
+		Assert.assertFalse(CollectionsUtils.equals(coll, arraySameSize));
+		// Not same content size
+		Assert.assertFalse(CollectionsUtils.equals(coll, arrayDifferentSize));
 	}
 
 	@Test
-	public void secondIsNull() {
-		Assert.assertFalse(CollectionsUtils.equals(null, Arrays.asList(1, 2, 3)));
-	}
+	public void mapTests() {
+		final Map<Integer, Integer> mapLeft = new HashMap<>();
+		final Map<Integer, Integer> mapRight = new TreeMap<>();
+		final Map<Integer, Integer> mapSameSize = new HashMap<>();
+		final Map<Integer, Integer> mapDifferentSize = new TreeMap<>();
+		final Map<Integer, Integer> nullMap = null;
 
-	@Test
-	public void notSameContent() {
-		Assert.assertFalse(CollectionsUtils.equals(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1)));
-	}
-
-	@Test
-	public void sameContentCollColl() {
-		Assert.assertTrue(CollectionsUtils.equals(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3)));
-	}
-
-	@Test
-	public void sameContentCollArray() {
-		Assert.assertTrue(CollectionsUtils.equals(Arrays.asList(1, 2, 3), new Integer[] { 1, 2, 3 }));
-	}
-
-	@Test
-	public void sameMap() {
-		Map map1 = new HashMap<Integer, Integer>();
-		Map map2 = new TreeMap<Integer, Integer>();
 		for (int i = 0; i < 5; i++) {
 			int value = RandomUtils.nextInt();
-			map1.put(value, value);
-			map2.put(value, value);
+			mapLeft.put(value, value);
+			mapRight.put(value, value);
+			mapSameSize.put(RandomUtils.nextInt(), RandomUtils.nextInt());
+			mapDifferentSize.put(value, value);
 		}
-		Assert.assertTrue(CollectionsUtils.equals(map1, map2));
+		mapDifferentSize.put(RandomUtils.nextInt(), RandomUtils.nextInt());
+
+		// Same null
+		Assert.assertTrue(CollectionsUtils.equals(nullMap, nullMap));
+		// Same reference
+		Assert.assertTrue(CollectionsUtils.equals(mapLeft, mapLeft));
+		// Same content
+		Assert.assertTrue(CollectionsUtils.equals(mapLeft, mapRight));
+		// Null pos2
+		Assert.assertFalse(CollectionsUtils.equals(mapLeft, nullMap));
+		// Null pos1
+		Assert.assertFalse(CollectionsUtils.equals(nullMap, mapRight));
+		// Not same content
+		Assert.assertFalse(CollectionsUtils.equals(mapLeft, mapSameSize));
+		// Not same content size
+		Assert.assertFalse(CollectionsUtils.equals(mapLeft, mapDifferentSize));
 	}
 
 	@Test
 	public void multilinePrint() {
 		Assert.assertEquals(String.format("one%ntwo%n%nthree"),
-							CollectionsUtils.multiline(Arrays.asList("one", "two", null, "three")));
+				CollectionsUtils.multiline(Arrays.asList("one", "two", null, "three")));
 	}
 
 	@Test
