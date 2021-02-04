04b8567c7b67eeae0f2fc3227987ac952dd1cd7e
Emmanuel Keller <ekeller@open-search-server.com>
2021-02-04 23:47:52 +0000

    Implements blocking executor service

--- src/main/java/com/qwazr/utils/Bitmap64Provider.java
+++ src/main/java/com/qwazr/utils/Bitmap64Provider.java
@@ -1,14 +1,19 @@
 /*
- * Copyright 2018-2019 SEBA
- *
- * This file is part of the SEBA AML project.
- * The SEBA AML project is not open-source software. It is owned by SEBA.
- * The SEBA AML project can not be copied and/or distributed without the express permission of SEBA.
- * Any form of modification or reverse-engineering of the SEBA AML project is forbidden.
+ * Copyright 2015-2020 Emmanuel Keller / QWAZR
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qwazr.utils;
-
-import org.roaringbitmap.longlong.Roaring64NavigableMap;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -24,6 +29,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import org.roaringbitmap.longlong.Roaring64NavigableMap;
 
 public class Bitmap64Provider implements
         MessageBodyReader<Roaring64NavigableMap>,
